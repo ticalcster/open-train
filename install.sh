836c4886d668d6a4c6b53cc530a251e253e82885
Kevin Clark <kclark@edustaff.org>
2017-12-13 22:12:55 -0500

    test

--- install.sh
+++ install.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 
+# Run as root or insert `sudo -E` before `bash`:
+#
+# curl -sL https://raw.githubusercontent.com/ticalcster/open-train/master/install.sh | bash -
+#   or
+# wget -qO- https://raw.githubusercontent.com/ticalcster/open-train/master/install.sh | bash -
+#
 
-cat > ./test.txt <<EOI
-Hello World
-This is the second
-and third lines
+cat > /opt/test.py <<EOI
+import time
+
+while True:
+    time.sleep(2)
+    print('I am in the loop')
 EOI
+
+
+cat > /lib/systemd/system/train.service <<EOI
+[Unit]
+Description=Open Train
+After=multi-user.target
+
+[Service]
+Type=idle
+ExecStart=/usr/bin/python3 /opt/test.py > /var/log/train.log 2>&1
+
+[Install]
+WantedBy=multi-user.target
+EOI
+
+chmod 644 /lib/systemd/system/train.service
+systemctl daemon-reload
+systemctl enable train.service
+systemctl status train.service
 
 # |
